3983d297786a9dcfe13ba02b511549776b3ab0fb
ran@panorays.com <ran@panorays.com>
2017-11-22 18:37:11 +0200

    Bump version: 0.1.8 → 0.1.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='auto_changelog',
-    version='0.1.8',
+    version='0.1.9',
     description='A quick script to generate changelogs from git commit messages',
     long_description=open('README.rst').read(),
     author='Ran Shamay',
